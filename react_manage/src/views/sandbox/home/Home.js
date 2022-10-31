251fac51145e8977a311b7c7419958891757aa73
zouxiangdong <zouxiangdong@xiaomi.com>
2022-10-31 20:38:26 +0800

    feat: 测试scss

--- react_manage/src/views/sandbox/home/Home.js
+++ react_manage/src/views/sandbox/home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Button } from 'antd';
+import TestScss from '../testScss/TestScss';
 
 export default function Home() {
   return (
     <div>
       home
       <Button type="primary">Button</Button>
-
+      <TestScss/>
     </div>
   )
 }
---
+++ react_manage/src/views/sandbox/testScss/TestScss.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './TestScss.scss'
+export default function TestScss() {
+  return (
+    <div  className='testScssBack'>
+      <div className='testScssBack_header'></div>
+      <div className='testScssBack_content'></div>
+      <div className='testScssBack_foot'></div>
+    </div>
+  )
+}
