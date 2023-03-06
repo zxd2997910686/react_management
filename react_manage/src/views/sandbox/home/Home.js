337391b0d80dfeff2f999402ec0d0a0ce9b8ac4e
zouxiangdong <zouxiangdong@xiaomi.com>
2023-03-06 19:35:19 +0800

    feat: 首页柱状图

--- react_manage/src/views/sandbox/home/Home.js
+++ react_manage/src/views/sandbox/home/Home.js
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Button ,Card,Col,Row,List,Avatar} from 'antd';
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import TestScss from '../testScss/TestScss';
 import axios from 'axios';
+import * as Echarts from 'echarts'
+import _ from 'lodash'
 const {Meta} = Card;
 export default function Home() {
   const [viewList,setViewList] = useState([])
   const [starLit,setStarList] = useState([])
+  const barRef = useRef()
   useEffect(()=>{
     axios.get("/news?publishState=2&_expand=category&_sort=view&_order=desc&_limit=6").then(res=>{
       // console.log("home-view--",res);
@@ -19,10 +22,54 @@ export default function Home() {
       setStarList(res.data)
     })
   },[])
+  useEffect(()=>{
+    axios.get("/news?publishState=2&_expand=category").then(res=>{
+      console.log('图状数据');
+     // console.log(res.data);
+      console.log(_.groupBy(res.data,item=> item.category.title));
+      renderBarView(_.groupBy(res.data,item=> item.category.title))
+    })
+    return ()=>{
+      window.onresize = null
+    }
+  },[])
 
+  const renderBarView = (obj)=>{
+    var myChart = Echarts.init(barRef.current);
+    //指定图标的配置项和数据
+    var option = {
+      title:{
+        text:'新闻分类图示'
+      },
+      tooltip:{},
+      legend:{
+        data:['数量']
+      },
+      xAxis:{
+        data:Object.keys(obj),
+        axisLabel:{
+          rotate:'45',
+          interval:0
+        }
+      },
+      yAxis:{
+        minInterval:1// 最小间隔
+      },
+      series:[{
+        name:'数量',
+        type:'bar',
+        data:Object.values(obj).map(item=> item.length)
+      }]
+    };
+    //使用刚制定的配置项和数据显示图表
+    myChart.setOption(option)
+
+    window.onresize=()=>{
+      myChart.resize()
+    }
+  }
   const {username,region,role:{roleName}} = JSON.parse(localStorage.getItem("token"))
   return (
-    <div>
       <div className='site-card-wrapper'>
         <Row gutter={16}>
           <Col span={8}>
@@ -72,7 +119,12 @@ export default function Home() {
             </Card>
           </Col>
         </Row>
+
+
+        <div ref={barRef} style = {{
+          width:'100%',height:'400px',marginTop:'30px'
+        }}>
+        </div>
       </div>
-    </div>
   )
 }
