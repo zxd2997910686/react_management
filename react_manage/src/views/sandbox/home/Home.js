ceb220650e67a19a946b2b07d88ee6cbfb9f07b0
zouxiangdong <zouxiangdong@xiaomi.com>
2023-03-06 18:32:19 +0800

    feat: 首页数据

--- react_manage/src/views/sandbox/home/Home.js
+++ react_manage/src/views/sandbox/home/Home.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button ,Card,Col,Row,List,Avatar} from 'antd';
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import TestScss from '../testScss/TestScss';
-
+import axios from 'axios';
 const {Meta} = Card;
 export default function Home() {
+  const [viewList,setViewList] = useState([])
+  const [starLit,setStarList] = useState([])
+  useEffect(()=>{
+    axios.get("/news?publishState=2&_expand=category&_sort=view&_order=desc&_limit=6").then(res=>{
+      // console.log("home-view--",res);
+      setViewList(res.data)
+    })
+  },[])
+  useEffect(()=>{
+    axios.get('/news?publishState=2&_expand=category&_sort=star&_order=desc&_limit=6').then(res=>{
+      // console.log('home--star--',res);
+      setStarList(res.data)
+    })
+  },[])
+
+  const {username,region,role:{roleName}} = JSON.parse(localStorage.getItem("token"))
   return (
     <div>
       <div className='site-card-wrapper'>
@@ -13,18 +29,22 @@ export default function Home() {
             <Card title = "用户最长浏览" bordered = {true}>
             <List
                 size='small'
-                 dataSource={["111","222","333"]}
-                  renderItem = {item => <List.Item>{item}</List.Item>}
+                 dataSource={viewList}
+                  renderItem = {item => <List.Item>
+                    <a href={`#/news-manage/preview/${item.id}`}>{item.title}</a>
+                  </List.Item>}
             />
             </Card>
           </Col>
           <Col span={8}>
             <Card  title = "用户点赞最多" bordered = {true}>
             <List
-                     size="small"
-                        // bordered
-                      dataSource={["1111", "222", "33333"]}
-                      renderItem={item => <List.Item>{item}</List.Item>}
+                  size="small"
+                  // bordered
+                  dataSource={starLit}
+                  renderItem={item => <List.Item>
+                      <a href={`#/news-manage/preview/${item.id}`}>{item.title}</a>
+                  </List.Item>}
              />
             </Card>
           </Col>
@@ -41,8 +61,13 @@ export default function Home() {
             >
               <Meta 
                  avatar = {<Avatar src = "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
-                 title = "card title"
-                 description = "This is the description"
+                 title = {username}
+                 description = {
+                  <div>
+                    <b>{region?region:"全球"}</b>
+                    <span style={{paddingLeft:"30px"}}>{roleName}</span>
+                  </div>
+                 }
               />
             </Card>
           </Col>
